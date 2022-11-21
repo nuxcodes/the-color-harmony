8bfde6150c2afede1e1e1e1c44dbe806d4639506
nuxcodes <44252050+nuxcodes@users.noreply.github.com>
2022-11-20 18:01:41 -0800

    add basic interactivity

--- main.js
+++ main.js
@@ -2,36 +2,135 @@
 // All the code below is javascript that's compiled into a shader
 
 
-
-
-
 import { sculptToMinimalRenderer } from 'https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js';
 import { spCode } from './spCode.js';
 
 let canvas = document.querySelector('.my-canvas');
-let canvas2 = document.querySelector('.canvas');
+let canvas2 = document.querySelector('div');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-let state = {
-    x: 1,
-    y: 1,
-};
-let test = 5;
 
-// canvas.addEventListener('mouseover', () => state.buttonHover = 5, false);
-// canvas.addEventListener('mouseout', () => state.buttonHover = 0.0, false);
-// canvas.addEventListener('mousedown', () => state.click = 1.0, false);
-// canvas.addEventListener('mouseup', () => state.click = 0.0, false);
+class Ball {
+    R = 0;
+    G = 0;
+    B = 0;
+    x0 = 0;
+    y0 = 0;
+    x1 = 0;
+    y1 = 0;
+    x2 = 0;
+    y2 = 0;
+    x3 = 0;
+    y3 = 0;
+    x4 = 0;
+    y4 = 0;
+    x5 = 0;
+    y5 = 0;
+}
+
+const colors = { cyan: [0, 1, 1], red: [1, 0, 0], green: [0, 1, 0], violet: [1, 0, 1], yellow: [1, 1, 0], blue: [0, 0, 1] };
+const dist = 1.44;
+
+// random int from [min, max]
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+class BallController {
+    ball;
+    record;
+    blobScale = 0.5;
+    dist = 1.44;
+    speed = 0.02;
+
+    constructor(ball) {
+        this.ball = ball;
+        this.record = [];
+        for (let i = 0; i < 6; i++) {
+            this.record.push(new BallCount(0, this.dist))
+            this.setPos(i, 0);
+        }
+        this.init()
+    }
+
+    mix(input) {
+        let res = [this.ball.R + input[0], this.ball.G + input[1], this.ball.B + input[2]].map((item) => item / 2);
+        this.ball.R = res[0];
+        this.ball.G = res[1];
+        this.ball.B = res[2];
+        return res;
+    }
+
+    init() {
+        let times = random(1, 5);
+        let values = Object.values(colors);
+        for (let i = 0; i < times; i++) {
+            this.mix(values[random(0, 5)])
+        }
+
+    }
+
+    animate(i) {
+        this.record[i].dist -= this.speed;
+        this.setPos(i, this.record[i].dist)
+        if (this.record[i].dist < 0.2) {
+            console.log("Animation stopped");
+            this.record[i].animating = false;
+            this.setPos(i, 0);
+            this.mix(Object.values(colors)[i]);
+        }
+    }
+
+    setPos(i, len) {
+        let angle = i * Math.PI / 3;
+        this.ball['x' + i.toString()] = len * Math.cos(angle);
+        this.ball['y' + i.toString()] = len * Math.sin(angle);
+    }
+
+}
+
+class BallCount {
+    count;
+    dist;
+    animating;
+    constructor(count, dist) {
+        this.count = count;
+        this.dist = dist;
+        this.animating = false;
+    }
+}
+
+
+let ball = new Ball();
+let ballController = new BallController(ball);
+console.log(ball.R);
+console.log(ball.G);
+console.log(ball.B);
+
+
+canvas2.addEventListener('click', () => {
+    let i = random(0, 5);
+    if (ballController.record[i].animating === true)
+        return;
+    else {
+        // set initial position
+        ballController.setPos(i, dist)
+        ballController.record[i].animating = true;
+        ballController.record[i].dist = dist;
+        ballController.record[i].count += 1;
+    }
+
+});
 
 // This converts your Shader Park code into a shader and renders it to the my-canvas element
 sculptToMinimalRenderer(canvas, spCode, () => {
-    state.x -= 0.001;
-    state.y -= 0.001;
-    return {
-        'x': state.x,
-        'y': state.y,
-    };
+
+    for (let [i, val] of ballController.record.entries()) {
+        if (val.animating)
+            ballController.animate(i)
+    }
+    return ball;
 });
 
 // StackBlur.canvasRGB(canvas2, 0, 0, width, height, 10);
--- spCode.js
+++ spCode.js
@@ -1,23 +1,84 @@
 
 export function spCode() {
-
     setMaxIterations(81342);
     setStepSize(0.8);
-    let x = input();
-    let y = input();
-    let offset = .1//input(0.08, 0, 0.1);
+    let balls = {
+        R: 0,
+        G: 0,
+        B: 0,
+        x0: 0,
+        y0: 0,
+        x1: 0,
+        y1: 0,
+        x2: 0,
+        y2: 0,
+        x3: 0,
+        y3: 0,
+        x4: 0,
+        y4: 0,
+        x5: 0,
+        y5: 0,
+    }
+
+    let R = input();
+    let G = input();
+    let B = input();
+    let x0 = input();
+    let y0 = input();
+    let x1 = input();
+    let y1 = input();
+    let x2 = input();
+    let y2 = input();
+    let x3 = input();
+    let y3 = input();
+    let x4 = input();
+    let y4 = input();
+    let x5 = input();
+    let y5 = input();
+
+    balls.R = R;
+    balls.G = G;
+    balls.B = B;
+    balls.x0 = x0;
+    balls.y0 = y0;
+    balls.x1 = x1;
+    balls.y1 = y1;
+    balls.x2 = x2;
+    balls.y2 = y2;
+    balls.x3 = x3;
+    balls.y3 = y3;
+    balls.x4 = x4;
+    balls.y4 = y4;
+    balls.x5 = x5;
+    balls.y5 = y5;
+
+    const colors = { cyan: [0, 1, 1], red: [1, 0, 0], green: [0, 1, 0], violet: [1, 0, 1], yellow: [1, 1, 0], blue: [0, 0, 1] };
+    const colorVals = Object.values(colors);
+
+    // let balls = new Ball();
+    // let offset = .1//input(0.08, 0, 0.1);
     noLighting();
     let scale = .5;
+    // draw background box
     color(vec3(0.5, 0.5, 0.5))
     displace(0, 0, 4);
     rotateY(0);
     box(44, 44, 0.2);
     reset();
-    color(vec3(0.1, 0.5, 0.5))
+    // draw central blob
+    color(vec3(balls.R, balls.G, balls.B))
     sphere(scale)
-    blend(.1);
-    // draw input sphere
-    displace(x, y, 0)
-    color(vec3(0, 1, 0))
-    sphere(.2)
+    blend(.4);
+    // draw input blob
+    for (let i = 0; i < 6; i++) {
+        let vals = Object.values(balls);
+        if (vals[2 * i + 3] != 0 && vals[2 * i + 4]) {
+            console.log("drawing ball");
+            displace(vals[2 * i + 3], vals[2 * i + 4], 0)
+            color(vec3(colorVals[i][0], colorVals[i][1], colorVals[i][2]))
+            sphere(.2)
+            reset()
+        }
+    }
+
 }
